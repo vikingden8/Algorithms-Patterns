dc749d55a6342e56449cd439e691d8354283776d
Viking Den <vikingden7@gmail.com>
2017-02-17 23:37:27 +0800

    ReverseInsertSort

--- src/main/java/com/viking/algorithms/sorting/ReverseInsertSort.java
+++ src/main/java/com/viking/algorithms/sorting/ReverseInsertSort.java
@@ -6,39 +6,36 @@ import com.viking.algorithms.util.BasicSorting;
  * Author : Viking Den <vikingden7@gmail.com>
  * Date : 2017/2/17
  *
- * 插入排序算法
- *
  * i   j      0    1   2   3   4   5
  *            R    G   A   F   V   M
- * 1   0     [G]   R   A   F   V   M
- * 2   0     [A]   G   R   F   V   M
- * 3   1      A   [F]  G   R   V   M
- * 4   4      A    F   G   R  [V]  M
- * 5   5      A    F   G   M   R  [V]
+ * 4   5      R    G   A   F   M  [V]
+ * 3   3      R    G   A  [F]  M   V
+ * 2   2      R    G  [A]  F   M   V
+ * 1   3      R    A   F  [G]  M   V
+ * 0   4      A    F   G   M  [R]  V
  *
  *            A    F   G   M   R   V
  */
-public class InsertSort extends BasicSorting{
-
+public class ReverseInsertSort extends BasicSorting{
 
     @Override
     public void sort(Comparable[] a) {
         int N = a.length ;
-        for (int i = 1 ; i < N ; i++){
-            for (int j = i ; j > 0 && less(a[j] , a[j-1]) ; j--){
-                exchange(a , j , j-1);
+        if (N <= 1) return;
+        for (int i = N-2 ; i >= 0 ; i--){
+            for (int j = i  ; j < N-1 && less(a[j+1] , a[j]) ; j++){
+                exchange(a , j , j+1);
             }
         }
     }
 
     public static void main(String[] args){
         String[] nums = new String[]{"R" , "G" , "A" , "F" , "V" , "M"} ;
-        BasicSorting sSort = new InsertSort() ;
+        BasicSorting sSort = new ReverseInsertSort() ;
         System.out.println("Before Sort");
         show(nums);
         sSort.sort(nums);
         System.out.println("After Sort");
         show(nums);
     }
-
 }
