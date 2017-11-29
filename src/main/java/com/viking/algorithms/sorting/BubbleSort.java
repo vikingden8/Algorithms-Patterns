91f58b3b4ad39a72b4c8e5d4d320cfe80f26d3f1
Viking Den <vikingden7@gmail.com>
2017-11-29 21:13:24 +0800

    add optiSort method

--- src/main/java/com/viking/algorithms/sorting/BubbleSort.java
+++ src/main/java/com/viking/algorithms/sorting/BubbleSort.java
@@ -21,12 +21,37 @@ public class BubbleSort extends BasicSorting {
         }
     }
 
+    public void optiSort(Comparable[] a) {
+        int N = a.length ;
+        for (int i = 0 ; i < N ; i++){
+            System.out.println("---------" + i + "-----------");
+            show(a);
+            boolean exchanged = false ;
+            for (int j = 0 ; j < N - i - 1 ; j++){
+                if (more(a[j] , a[j+1])) {
+                    exchange(a, j, j+1);
+                    exchanged = true ;
+                }
+            }
+            if (!exchanged){
+                break ;
+            }
+        }
+    }
+
     public static void main(String[] args){
         String[] nums = new String[]{"R" , "G" , "A" , "F" , "V" , "M"} ;
         BubbleSort sSort = new BubbleSort() ;
         System.out.println("Before Sort");
         show(nums);
         sSort.sort(nums);
+        System.out.println("After Sort");
+        show(nums);
+
+        nums = new String[]{"R" , "A" , "F" , "G" , "V" , "M"} ;
+        System.out.println("Before Sort");
+        show(nums);
+        sSort.optiSort(nums);
         System.out.println("After Sort");
         show(nums);
     }
